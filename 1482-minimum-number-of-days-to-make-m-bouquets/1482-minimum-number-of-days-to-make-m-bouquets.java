7b8b32bbbb4358d596b5d6aa95853720cdae5b4a
ayush807sinha <112458566+ayush807sinha@users.noreply.github.com>
2026-01-14 21:03:46 +0530

    Time: 21 ms (27.64%), Space: 82.7 MB (50.93%) - LeetHub

---
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
@@ -0,0 +1,55 @@
+class Solution {
+
+    public static boolean find(int arr[],int m,int k,int n,int guess){
+        int cnt=0;
+        int sum=0;
+
+        for(int i =0;i<n;i++){
+            if(arr[i]<=guess){
+                cnt++;
+            }else{
+                sum = sum + (cnt/k);
+                cnt=0;
+            }
+        }
+
+        sum = sum + (cnt/k);
+
+        if(sum>=m){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public int minDays(int[] arr, int m, int k) {
+        int n = arr.length;
+        int low =0;
+        for(int i=0;i<n;i++){
+            if(arr[i]<=low){
+                low =arr[i];
+            }
+        }
+        int high = 0;
+        for(int i=0;i<n;i++){
+            if(arr[i]>=high){
+                high = arr[i];
+            }
+        }
+        int ans = -1;
+        while(low <=high){
+            int guess = (low+high)/2;
+
+            boolean tempans = find(arr,m,k,n,guess);
+
+            if(tempans){
+                ans = guess;
+                high =guess-1; 
+            }else{
+                low = guess+1;
+            }
+
+        }
+
+        return ans;
+    }
+}
