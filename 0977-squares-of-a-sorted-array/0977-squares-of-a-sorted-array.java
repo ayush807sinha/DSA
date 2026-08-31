563551bf7512fe0dad0a8a93323befedb8ab4240
ayush807sinha <112458566+ayush807sinha@users.noreply.github.com>
2026-08-31 11:15:04 +0530

    Time: 1 ms (100%), Space: 47.5 MB (43.03%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -3,11 +3,66 @@ class Solution {
         
         int n = arr.length;
 
+        int poscnt = 0;
+        int negcnt = 0;
+
         for(int i=0;i<n;i++){
-            arr[i] = arr[i] * arr[i];
+            if(arr[i]>=0){
+                poscnt++;
+            }else{
+                negcnt++;
+            }
         }
 
-        Arrays.sort(arr);
+        int pos[] = new int[poscnt];
+        int neg[] = new int[negcnt];
+        
+        
+
+        int i = 0;
+        int j = negcnt-1;
+        
+        
+        for(int k = 0;k<n;k++){
+                
+            if(arr[k]>=0){
+                pos[i] = arr[k] * arr[k];
+                i++;
+            }else{
+                neg[j] = arr[k] * arr[k];
+                j--;
+            }    
+            
+        }
+
+        i = 0;
+        j = 0;
+        int k = 0;
+
+        while(i<pos.length && j <neg.length){
+            if(pos[i] <= neg[j]){
+                arr[k] = pos[i];
+                k++;
+                i++;
+            }else{
+                arr[k] =  neg[j];
+                k++;
+                j++;
+            }
+        }
+
+        while(i<pos.length){
+            arr[k] = pos[i];
+            i++;
+            k++;
+        }
+
+
+        while(j<neg.length){
+            arr[k] = neg[j];
+            j++;
+            k++;
+        }
 
         return arr;
 
