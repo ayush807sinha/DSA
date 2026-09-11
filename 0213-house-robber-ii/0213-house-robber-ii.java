8658862f419abfb6a93280b158a7cb079fbf4f77
ayush807sinha <112458566+ayush807sinha@users.noreply.github.com>
2026-09-11 08:28:04 +0530

    Time: 0 ms (100%), Space: 42.7 MB (77.65%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,42 +1,36 @@
 class Solution {
-    public static int find(int arr[]){
+    public int rob(int[] arr) {
+
         int n = arr.length;
-        if(n==0){
-            return 0;
-        }
-        if(n==1){
-            return arr[0];
-        }
 
         int dp[] = new int[n];
+
         dp[0] = arr[0];
-        dp[1] = Math.max(arr[0],arr[1]);
-
-        for(int i=2;i<n;i++){
-            int pick = arr[i] + dp[i-2];
-            int notpick = 0 + dp[i-1];
-            dp[i] = Math.max(pick,notpick);
-        }
-
-        return dp[n-1];
-        
-    }
-    public int rob(int[] arr) {
-        int n = arr.length;
         if(n==1){
             return arr[0];
         }
-        int temp1[] = new int[n-1];
-        int temp2[] = new int[n-1];
+        dp[1] = arr[0];
 
-        for(int i =0;i<temp1.length;i++){
-            temp1[i] = arr[i];
-            temp2[i] = arr[i+1];
+        for(int i=2;i<=n-2;i++){
+            int c1 = arr[i] + dp[i-2];
+            int c2  = dp[i-1];
+            dp[i] = Math.max(c1,c2);
         }
 
-        int ans1 = find(temp1);
-        int ans2 =  find(temp2);
+        int ans1 = dp[n-2];
 
-        return Math.max(ans1,ans2);
+        dp[1] = arr[1];
+        dp[0] = 0;
+
+        for(int i=2;i<=n-1;i++){
+            int c1 = arr[i] + dp[i-2];
+            int c2 = dp[i-1];
+            dp[i] = Math.max(c1,c2);
+        }
+
+        int ans2 = dp[n-1];
+
+        return Math.max(ans1,ans2);  
+        
     }
 }
